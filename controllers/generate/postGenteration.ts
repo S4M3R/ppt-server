48ac1bf986684fa7bfa6f0ca31c869d4e8d50bd1
Samer <samerishappy@gmail.com>
2025-04-10 08:27:48 -0700

    Started with generate slide

--- controllers/generate/postGenteration.ts
+++ controllers/generate/postGenteration.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
+import PptxGenJS from "pptxgenjs";
 
 const postGeneration = async (req: Request, res: Response) => {
+  const pptx = new PptxGenJS();
+//   pptx.createPptx();
+  pptx.addSlide();
+  pptx.writeFile({ fileName: "test.pptx" });
   res.status(200).send("Generation successful");
 };
 
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import templatesRouter from "./routes/templates";
+import generateRouter from "./routes/generate";
 
 dotenv.config();
 
@@ -8,6 +10,10 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI as string);
 
+app.use(express.json());
+
+app.use("/templates", templatesRouter);
+app.use("/generate", generateRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
